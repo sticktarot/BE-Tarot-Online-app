87fdab80c57b49f4e098c0c5bd205d38e4e18469
longtn <long.tn@mor.com.vn>
2024-12-13 14:38:43 +0700

    first initialize

---
+++ Tarot-application/src/main/java/Tarot_app/Tarot_application/TarotApplication.java
@@ -0,0 +1,13 @@
+package Tarot_app.Tarot_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarotApplication.class, args);
+	}
+
+}
